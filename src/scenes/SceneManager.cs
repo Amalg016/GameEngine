c64f61b0f90331df2aa71e21b731481b1e260a98
Amalg016 <amalgjoseph1@gmail.com>
2025-10-11 22:51:23 +0530

    Added SceneManager

---
+++ src/scenes/SceneManager.cs
@@ -0,0 +1,62 @@
+using GameEngine.observers.events;
+using GameEngine.components;
+
+namespace GameEngine.scenes
+{
+    public class SceneManager
+    {
+        private static Scene _currentScene;
+        private static bool _runtimePlaying = false;
+        private DirectoryInfo _scenePath = new DirectoryInfo("Assets/Scenes/Lvl1.json");
+
+        public static Scene CurrentScene => _currentScene;
+        public static bool RuntimePlaying => _runtimePlaying;
+
+        public void ChangeScene(sceneInitializer sceneInitializer)
+        {
+            _currentScene?.Destroy();
+            Component.ID_Counter = 0;
+
+            _currentScene = new Scene(sceneInitializer);
+            _currentScene.init();
+            _currentScene.LoadLevelResources(_scenePath.FullName);
+            _currentScene.Start();
+        }
+
+        public void HandleEngineEvent(Event gameEvent)
+        {
+            switch (gameEvent.Type)
+            {
+                case EventType.GameEngineStartPlay:
+                    _runtimePlaying = true;
+                    _currentScene?.SaveResources();
+                    ChangeScene(new LevelEditorSceneInitializer());
+                    break;
+                case EventType.GameEngineStopPlay:
+                    _runtimePlaying = false;
+                    ChangeScene(new LevelEditorSceneInitializer());
+                    break;
+                case EventType.LoadLevel:
+                    ChangeScene(new LevelEditorSceneInitializer());
+                    break;
+                case EventType.SaveLevel:
+                    _currentScene?.SaveResources();
+                    break;
+            }
+        }
+
+        public void SaveCurrentScene()
+        {
+            if (!_runtimePlaying)
+            {
+                _currentScene?.SaveResources();
+            }
+        }
+
+        public void ExitScene()
+        {
+            SaveCurrentScene();
+            _currentScene?.Exit();
+        }
+    }
+}
